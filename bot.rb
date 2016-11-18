9a220f562c857d4843c28ab08988d860ecefc55b
Loïc Delmaire <loic.delmaire@gmail.com>
2016-11-18 23:03:52 +0100

    much config

--- bot.rb
+++ bot.rb
@@ -1,5 +1,10 @@
 require 'facebook/messenger'
 
+Facebook::Messenger.configure do |config|
+  config.access_token = ENV['ACCESS_TOKEN']
+  config.verify_token = ENV['VERIFY_TOKEN']
+end
+
 include Facebook::Messenger
 
 BASE_URL = 'http://dogr.io/'
